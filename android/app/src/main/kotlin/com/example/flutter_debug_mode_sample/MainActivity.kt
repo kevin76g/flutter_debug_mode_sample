1fcb4c5342e4974227422b7cc8ec549ff2c36a22
kevin76g <harada@elwice.jp>
2022-08-14 14:03:26 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_debug_mode_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_debug_mode_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextStyle textStyle = const TextStyle(
+      fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.black);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          const SizedBox(
+            height: 50.0,
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'メニュー1',
+              style: textStyle,
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'メニュー2',
+              style: textStyle,
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'メニュー3',
+              style: textStyle,
+            ),
+          ),
+          Visibility(
+              visible: kDebugMode,
+              child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const AdminScreen()),
+                    );
+                  },
+                  child: Text(
+                    '管理者画面へ',
+                    style: TextStyle(
+                        fontSize: textStyle.fontSize,
+                        fontWeight: textStyle.fontWeight,
+                        color: Colors.blue),
+                  ))),
+        ]),
+      ),
+    );
+  }
+}
+
+class AdminScreen extends StatefulWidget {
+  const AdminScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AdminScreen> createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
